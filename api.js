6bd24934c8a0f92135aef752aa851ef63671ba6f
Kim Boender <kim.boender.dev>
2022-08-21 14:48:58 +0200

    Weg ermee!

--- api.js
+++ api.js
@@ -7,17 +7,3 @@ module.exports = getListings = () => {
         },
     });
 };
-module.exports = getListings = () => {
-    return axios.get('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
-        headers: {
-            'X-CMC_PRO_API_KEY': 'YOUR_KEY_HERE_JOSZ',
-        },
-    });
-};
-module.exports = getListings = () => {
-    return axios.get('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
-        headers: {
-            'X-CMC_PRO_API_KEY': 'YOUR_KEY_HERE_JOSZ',
-        },
-    });
-};
